ce9092bde7c59d3c93a304dfa36cc848702e1add
vibhu karthikeya <potvy001@mymail.unisa.edu.au>
2025-11-16 21:09:21 +1030

    Initialising Staff and Animal classes

--- animal.py
+++ animal.py
@@ -1,8 +1,49 @@
 '''
-File: filename.py
+File: animal.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Vibhu Karthikeya Pothanaboina
+ID: 110446905
+Username: potvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    Abstract base Class for all animals
+    """
+    def __init__(self, name, species, age,
+                 dietary_needs , environment):
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__dietary_needs = dietary_needs
+        self.__required_environment = environment
+        self.__health_records = []  # To store health record
+
+class Mammal(Animal):
+    """
+    Represents a mammal, inheriting from Animal class
+    """
+    def __init__(self, name, species, age,
+                 dietary_needs, environment):
+        super().__init__(name, species, age, dietary_needs, environment)
+
+
+class Bird(Animal):
+    """
+    Represents a bird, inheriting from Animal class
+    """
+    def __init__(self, name, species, age,
+                 dietary_needs, environment):
+        super().__init__(name, species, age, dietary_needs, environment)
+
+
+class Reptile(Animal):
+    """
+    Represents a reptile, inheriting from Animal class
+    """
+    def __init__(self, name, species, age,
+                 dietary_needs, environment):
+        super().__init__(name, species, age, dietary_needs, environment)
--- staff.py
+++ staff.py
@@ -5,4 +5,28 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+from abc import ABC, abstractmethod
+
+class Staff(ABC):
+    """
+    Abstract Class for all staff members
+    """
+    def __init__(self, name, staff_id):
+        self.__name = name
+        self.__staff_id = staff_id
+
+class Zookeeper(Staff):
+    """
+    Represents a Zookeeper, inheriting from Staff
+    """
+    def __init__(self, name, staff_id):
+        super().__init__(name, staff_id)
+        self.__assigned_enclosures = [] # enclosure assigned to zookeper
+
+class Veterinarian(Staff):
+    """
+    Represents a Veterinarian, inheriting from Staff
+    """
+    def __init__(self, name, staff_id):
+        super().__init__(name, staff_id)
